ca9d7b706c0eeec6595f98804076a4474ddab8ba
subhransu das <subhransud525@gmail.com>
2021-05-09 16:46:52 +0530

    created Product & Purchase model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from products.models import Product,Purchase
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Purchase)
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['name','']
--- products/models.py
+++ products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(blank=True)
+    salesman = models.ForeignKey(User, on_delete=models.CASCADE)
+    # change required
+    date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.price * self.quantity
+        super(Purchase,self).save(*args, *kwargs)
+
+    def __str__(self):
+        return f"saled {self.product.name} - {self.quantity} items for {self.total_price}"
