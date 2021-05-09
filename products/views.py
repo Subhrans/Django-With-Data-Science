0b4554c372b7f10990a49fc719406dcba2bbf33f
subhransu das <subhransud525@gmail.com>
2021-05-09 21:29:42 +0530

    created dataframe with removed redundant columns

--- products/views.py
+++ products/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from .models import Product,Purchase
+from .models import Product, Purchase
 import pandas as pd
+
+
 # Create your views here.
 def index(request):
-    qs1=pd.DataFrame(Product.objects.all().values()).to_html
-    qs2=pd.DataFrame(Purchase.objects.all().values()).to_html
-    context={
-        'product':qs1,
-        'purchase':qs2,
+    qs1 = pd.DataFrame(Product.objects.all().values())
+    qs4 = pd.DataFrame(Product.objects.all().values()).to_html
+    qs2 = pd.DataFrame(Purchase.objects.all().values()).to_html
+    qs3 = pd.DataFrame(Purchase.objects.all().values())
+    qs1['product_id'] = qs1['id']
+    merged = pd.merge(qs1, qs3, on='product_id').drop(['id_x', 'date_x'],axis=1).to_html
+    context = {
+        'product': qs4,
+        'purchase': qs2,
+        'merged': merged,
     }
 
-    return render(request,'products/index.html',context)
+    return render(request, 'products/index.html', context)
