447de761fe5330035fd113fb19fe008ec05d090d
subhransu das <subhransud525@gmail.com>
2021-05-13 22:58:40 +0530

    data froms for DScience

---
+++ products/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+choice = (
+    ('bar plot', 'bar plot'),
+    ('line plot', 'line plot'),
+    ('graph plot', 'graph plot'),
+)
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+
+class DataForm(forms.Form):
+    choose_chart_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control form-select'}),
+                                          choices=choice)
+    date_from = forms.DateField(widget=DateInput(attrs={'class': 'form-control'}),)
+    date_to = forms.DateField(widget=DateInput(attrs={'class': 'form-control'}))
--- products/views.py
+++ products/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
 from .models import Product, Purchase
 import pandas as pd
+from .forms import DataForm
 
 
 # Create your views here.
 def index(request):
     qs1 = pd.DataFrame(Product.objects.all().values())
-    qs4 = pd.DataFrame(Product.objects.all().values()).to_html
-    qs2 = pd.DataFrame(Purchase.objects.all().values()).to_html
+    qs4 = pd.DataFrame(Product.objects.all().values()).to_html()
+    qs2 = pd.DataFrame(Purchase.objects.all().values()).to_html()
     qs3 = pd.DataFrame(Purchase.objects.all().values())
     qs1['product_id'] = qs1['id']
-    merged = pd.merge(qs1, qs3, on='product_id').drop(['id_x', 'date_x'],axis=1).to_html
+    merged = pd.merge(qs1, qs3, on='product_id').drop(['id_y', 'date_x'], axis=1).rename(
+        {'id_x': 'Id', 'date_y': 'Date'}, axis=1)
+    if request.method == "POST":
+        data_form=DataForm(request.POST)
+        if data_form.is_valid():
+            date_from=data_form.cleaned_data['date_from']
+            date_to=data_form.cleaned_data['date_to']
+            print("the date is",date_from)
+    else:
+        data_form = DataForm()
     context = {
         'product': qs4,
         'purchase': qs2,
-        'merged': merged,
+        'merged': merged.to_html(),
+        'data_form': data_form,
     }
 
     return render(request, 'products/index.html', context)
